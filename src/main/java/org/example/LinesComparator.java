bce7aa7165a4019150a06d7ba4c6686d7f1c0b04
Eleshka-Arr <elenka.arapova.99@mail.ru>
2022-08-27 14:40:51 +0700

    LinesComparator зарефакторен

--- src/main/java/org/example/LinesComparator.java
+++ src/main/java/org/example/LinesComparator.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Comparator;
 
 public class LinesComparator implements Comparator<String[]> {
-    private int row;
+    private final int row;
 
     LinesComparator(int row){
         this.row = row;
@@ -13,14 +13,13 @@ public class LinesComparator implements Comparator<String[]> {
     public int compare(String[] o1, String[] o2) {
         String word1 = o1[row];
         String word2 = o2[row];
-        Integer num1 = null;
-        Integer num2 = null;
+        Integer num1;
+        int num2;
 
         try {
             num1 = Integer.parseInt(word1);
             num2 = Integer.parseInt(word2);
-        }
-        catch (NumberFormatException exception) {
+        } catch (NumberFormatException exception) {
             return word1.compareTo(word2);
         }
 
