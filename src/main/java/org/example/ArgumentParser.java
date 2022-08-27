2959907dd3987771887f27f258cb18cf7a3be1b5
Eleshka-Arr <elenka.arapova.99@mail.ru>
2022-08-27 14:38:14 +0700

    ArgumentParser зарефакторен

--- src/main/java/org/example/ArgumentParser.java
+++ src/main/java/org/example/ArgumentParser.java
@@ -3,16 +3,19 @@ package org.example;
 public class ArgumentParser {
     public static int parseArgument(String[] args) {
         int row;
+
         try {
             row = Integer.parseInt(args[0]);
 
             if (row > 14 || row <= 0) {
                 throw new NumberFormatException();
             }
+
             row--; /* Нужно для смещения под индексацию массива */
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("Некорректно введен номер столбца");
         }
+
         return row;
     }
 }
